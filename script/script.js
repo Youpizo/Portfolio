025bb520a6ca91559cd8792f9ca554ebcf117945
Youpizo <loic.deruy@sfr.fr>
2025-02-28 10:28:10 +0100

    finalisation href

--- script/script.js
+++ script/script.js
@@ -75,12 +75,12 @@ function createPortfolioFromJSON() {
                 card.classList.add("col-lg-4", "mt-4");
                 card.innerHTML = `
                     <div class="card portfolioContent">
-                    <img class="card-img-top" src="images/${item.image}" style="width:100%">
+                    <img class="card-img-top" src="images/${item.image}" alt="${item.title}" style="width:100%">
                     <div class="card-body">
                         <h3 class="card-title">${item.title}</h3>
                         <p class="card-text">${item.text}</p>
                         <div class="text-center">
-                            <a href="${item.link}" class="btn btn-success">Lien</a>
+                            <a href="${item.link}" target="_blank" class="btn btn-success">Lien</a>
                         </div>
                     </div>
                 </div>
